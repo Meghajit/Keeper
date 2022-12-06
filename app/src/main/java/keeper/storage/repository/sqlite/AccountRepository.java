e90af8e2c830a3370c84836e8a0df517b818d406
Meghajit Mazumdar <Megh319@gmail.com>
2022-12-06 23:27:50 +0530

    feat: add init account and create account functions

--- app/src/main/java/keeper/storage/repository/sqlite/AccountRepository.java
+++ app/src/main/java/keeper/storage/repository/sqlite/AccountRepository.java
@@ -2,8 +2,7 @@ package keeper.storage.repository.sqlite;
 
 import keeper.entity.Account;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class AccountRepository {
     private final Connection connection;
@@ -12,14 +11,31 @@ public class AccountRepository {
         try {
             String dbPath = "app/src/main/resources/keeper.db";
             connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+            Statement statement = connection.createStatement();
+            System.out.println("AccountRepository DB connection established");
+            String sql = "CREATE TABLE IF NOT EXISTS ACCOUNT " +
+                    "(CUSTOMER_UUID TEXT NOT NULL," +
+                    " ENCRYPTED_PASSKEY BLOB NOT NULL, " +
+                    " PRIMARY KEY (CUSTOMER_UUID, ENCRYPTED_PASSKEY))";
+            statement.executeUpdate(sql);
+            statement.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        System.out.println("AccountRepository DB connection established");
     }
 
     public Account createAccount(String customerUUID, byte[] passKey) {
-        return null;
+        try {
+            String sql = "INSERT INTO ACCOUNT(CUSTOMER_UUID, ENCRYPTED_PASSKEY) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, customerUUID);
+            statement.setBytes(2, passKey);
+            System.out.println(statement.executeUpdate() + " records inserted");
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return new Account(customerUUID, passKey);
     }
 
     public Account findAccount(String customerUUID, byte[] passKey) {
