4640c360061fccf9d54b27319dc403b906887026
Meghajit Mazumdar <Megh319@gmail.com>
2022-12-08 01:50:57 +0530

    feat: add example usage

--- app/src/main/java/keeper/App.java
+++ app/src/main/java/keeper/App.java
@@ -3,12 +3,27 @@
  */
 package keeper;
 
+import keeper.service.TwoFishEncryptionService;
+import keeper.template.WebsiteTemplate;
+
+import java.nio.charset.StandardCharsets;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        TwoFishEncryptionService<WebsiteTemplate> enc = new TwoFishEncryptionService<>();
+        WebsiteTemplate ws = new WebsiteTemplate("Some".getBytes(StandardCharsets.UTF_8), "Password".getBytes(StandardCharsets.UTF_8), "no metadata".getBytes(StandardCharsets.UTF_8));
+        byte[] passkey = ("This is my passkey").getBytes(StandardCharsets.UTF_8);
+
+        // Encrypting the secret
+        byte[] cipherText = enc.encrypt(ws, passkey);
+        System.out.println("Cipher text is: " + new String(cipherText, StandardCharsets.UTF_8));
+
+        // Decrypting the secret
+        WebsiteTemplate decryptedSecret = enc.decrypt(cipherText, passkey);
+        System.out.println("Plain text is: " + decryptedSecret.print());
     }
 }
